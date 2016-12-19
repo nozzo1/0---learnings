9ba70678d8490917e59579a3d3d7ebed48952a2d
nozzo1 <nozzo1@gmail.com>
2016-12-19 16:26:15 +0000

    Added test point size - needs formatting

--- policedata3.R
+++ policedata3.R
@@ -3,15 +3,15 @@ library(mapproj);library( maps);library(jsonlite);library(ggmap);library(colorsp
 require(ggplot2); require(sp);
 
 # Setup
-# baseurl <- "https://data.police.uk/api/crimes-street/all-crime?lat=52.091983&lng=-0.835741&date=" #Castlethorpe
-# mappostcode <- "mk19 7hf" #castlethorpe
-# maptitle <- "Castlethorpe"
+baseurl <- "https://data.police.uk/api/crimes-street/all-crime?lat=52.091983&lng=-0.835741&date=" #Castlethorpe
+mappostcode <- "mk19 7hf" #castlethorpe
+maptitle <- "Castlethorpe"
 # baseurl <- "https://data.police.uk/api/crimes-street/all-crime?lat=52.0417200&lng=-0.7558300&date=" #Milton Keynes
 # mappostcode <- "mk9 3nb" #milton keynes
 # maptitle <- "Milton Keynes"
-baseurl <- "https://data.police.uk/api/crimes-street/all-crime?lat=51.8199&lng=-0.824&date=" #Aylesbury Honda
-mappostcode <- "hp19 8ed" #Aylesbury
-maptitle <- "Aylesbury"
+# baseurl <- "https://data.police.uk/api/crimes-street/all-crime?lat=51.8199&lng=-0.824&date=" #Aylesbury Honda
+# mappostcode <- "hp19 8ed" #Aylesbury
+# maptitle <- "Aylesbury"
 
 baseyear  <- "2016"
 poldatapages <- list()
@@ -35,9 +35,11 @@ poldata1$location.longitude <- as.numeric(as.character(poldata1$location.longitu
 # then merge the two tables so we end up with the location (lat,lon) count of crimes in polmerge3
 poltable1 <- table(poldata1$location.latitude, poldata1$location.longitude)
 polmerge3 <- merge(poldata1, poltable1, by.x=c("location.latitude", "location.longitude"), by.y=c("Var1", "Var2"))
+polmerge3$tFreq <- as.character(polmerge3$Freq)
 
 # open map and plot
-crimemap = qmap(mappostcode, zoom = 14, maptype='hybrid', size = c(1024, 768))
-crimemap = crimemap + geom_point(data = polmerge3, aes(x = location.longitude, y = location.latitude, size = Freq), color = "red")
-crimemap = crimemap + ggtitle(paste(maptitle, "Crime For year", baseyear))
+crimemap = qmap(mappostcode, zoom = 15, maptype='hybrid', size = c(1024, 768)) +
+  geom_point(data = polmerge3, aes(x = location.longitude, y = location.latitude, size = Freq), color = "red") +
+  geom_text(data = polmerge3, aes(x = location.longitude, y = location.latitude, label = Freq), size=4, vjust=.5, hjust=0.5, color = "white") +
+  ggtitle(paste(maptitle, "Crime For year", baseyear))
 print(crimemap)
